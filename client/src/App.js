1d4e3a7090e0469392dc2e466e45b7f4fcae0d0f
Skim1571 <skim1571@gmail.com>
2022-07-21 13:53:10 -0500

    got header working

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,13 @@
-import './App.css';
+import './styles/App.css';
 import { Routes, Route } from 'react-router-dom'
 import Test from './components/Test'
+import Header from './components/Header'
+
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <h1>This is working</h1>
       <Routes>
         <Route path='/test' element={<Test/>}/>
---
+++ client/src/pages/Home.js
@@ -0,0 +1,60 @@
+import { useEffect, useState } from 'react'
+import Search from '../components/Search'
+
+
+const Home = () => {
+  const [category, setCategories] = useState([])
+  const [searchResults, setSearchResults] = useState([])
+  const [searched, toggleSearched] = useState(false)
+  const [searchQuery, setSearchQuery] = useState('')
+
+
+// useEffect(()=>{
+//   const getGenres = async () => {
+//     let res = await axios.get(`https://api.rawg.io/api/genres?key=${process.env.REACT_APP_RAWG_KEY}`)
+//     setGenres(res.data.results)}
+//     getGenres()
+//   },[])
+
+//   const handleChange = (event) => {
+//     setSearchQuery( event.target.value)
+//   }
+  
+//   const getSearchResults = async (event) => {
+//     event.preventDefault()
+//     let res = await axios.get(`https://api.rawg.io/api/games?key=${process.env.REACT_APP_RAWG_KEY}&search=${searchQuery}`)
+//     setSearchResults(res.data.results)
+//     toggleSearched(true)
+//   }
+  
+let rideCard
+let Categorycard
+
+if (searched){
+  rideCard = <RideCard ride={searchResults}/>
+  categoryCard = <CategoryCard category={category}/>
+}
+
+  return (
+    <div>
+      <div className="search">
+        <Search 
+        onChange={handleChange}
+        onSubmit={getSearchResults}
+        />
+        <h2>Search Results</h2>
+        <section className="search-results container-grid">
+          {rideCard}
+        </section>
+      </div>
+      <div className="categories">
+        <h2>Categories</h2>
+        <section className="container-grid">
+          {categoriesCard}
+        </section>
+      </div>
+    </div>
+  )
+}
+
+export default Home
