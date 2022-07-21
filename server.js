7bbba2ee5c7d962850e275f08032c7e1cca6471c
Austin Taylor <austinrtaylor91@gmail.com>
2022-07-21 14:28:43 -0400

    created categories express route

--- server.js
+++ server.js
@@ -1,19 +1,26 @@
-const express = require('express')
-const cors = require('cors')
-const logger = require('morgan')
-const PORT = process.env.PORT || 3001
-const db = require('./db')
+const express = require('express');
+const cors = require('cors');
+const logger = require('morgan');
+const PORT = process.env.PORT || 3001;
+const db = require('./db');
 
-const app = express()
+const app = express();
 
-app.use(cors())
-app.use(express.json())
-app.use(logger('dev'))
+app.use(cors());
+app.use(express.json());
+app.use(logger('dev'));
+
+const { Categories, Rides } = require('./models');
 
 app.get('/', (req, res) => {
-  res.send('This is root!')
-})
+  res.send('Root route!');
+});
+
+app.get('/categories', async (req, res) => {
+  const categories = await Categories.find({});
+  res.json(categories);
+});
 
 app.listen(PORT, () => {
-  console.log(`Express server listening on port ${PORT}`)
-})
+  console.log(`Express server listening on port ${PORT}`);
+});
