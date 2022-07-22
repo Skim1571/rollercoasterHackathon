ca1d234989d875e44f12354ed96d64b188237028
Josh Langner <josh.langner@gmail.com>
2022-07-21 19:24:36 -0500

    logic for category display & home button component created

---
+++ client/src/pages/Categories.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import { useNavigate } from 'react-router-dom'
+import CategoryCard from '../components/CategoryCard'
+import HomeButton from '../components/HomeButton'
+
+const BASE_URL = 'http://localhost:3001'
+
+const Categories = (props) => {
+  let navigate = useNavigate()
+  return (
+    <div>
+      <ul className="category-item">
+        <h2 className="ride-type">Ride Type: {props.type}</h2>
+        <h3 className="intensity">Intensity Level: {props.intensity}</h3>
+        <h3 className="nausea">Nausea Level: {props.type}</h3>
+        <img src={props.image_background} />
+      </ul>
+      <div>
+        <CategoryCard />
+      </div>
+      <HomeButton />
+    </div>
+  )
+}
+
+export default Categories
