aa3fccdcae336b7629ef00e295e3327af28cc195
Skim1571 <skim1571@gmail.com>
2022-07-22 08:47:29 -0500

    updated user schema & updated home page

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,14 +1,9 @@
 import { useEffect, useState } from 'react';
-import RideCard from '../components/RideCard';
 import axios from 'axios';
 import CategoryCard from '../components/CategoryCard';
 
-
 const Home = () => {
   const [category, setCategories] = useState([]);
-  const [searchResults, setSearchResults] = useState([]);
-  const [searched, toggleSearched] = useState(false);
-  const [searchQuery, setSearchQuery] = useState('');
   const BASE_URL = 'http://localhost:3001';
 
   useEffect(() => {
@@ -19,29 +14,12 @@ const Home = () => {
     getCategories();
   }, []);
 
-  const handleChange = (event) => {
-    setSearchQuery(event.target.value);
-  };
-
-  const getSearchResults = async (event) => {
-    event.preventDefault();
-    let res = await axios.get(`${BASE_URL}/rides`);
-    console.log(`searchresults`, res);
-    setSearchResults(res.data);
-    toggleSearched(true);
-  };
-
-
   return (
     <div>
       <div className="categories">
         <h2>Ride Categories</h2>
         <section className="container-grid">
           <CategoryCard categories={category} />
-        </section>
-        <h2>Rides </h2>
-        <section className="container-grid">
-          <RideCard rides={searchResults} />
         </section>
       </div>
     </div>
